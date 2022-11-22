1da80125389e9e88678697f6ad75f0167e62f304
kuanoni <tehgamemaker@gmail.com>
2022-11-22 13:40:44 -0800

    add layout wrapper to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,15 @@
+import { NextComponentType } from 'next';
 import type { AppProps } from 'next/app';
+import { ReactElement, ReactNode } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+type LayoutPage = NextComponentType & { getLayout?: (page: ReactElement) => ReactNode };
+
+interface LayoutAppProps extends AppProps {
+	Component: LayoutPage;
+}
+
+export default function App({ Component, pageProps }: LayoutAppProps) {
+	const getLayout = Component.getLayout || ((page) => page);
+
+	return getLayout(<Component {...pageProps} />);
 }
