d6a15083af01f9d48f928f81c2623a0275544f00
kuanoni <tehgamemaker@gmail.com>
2022-11-22 13:45:25 -0800

    create blank pages

---
+++ src/pages/videos.tsx
@@ -0,0 +1,12 @@
+import LayoutWithNavbar from '@/layouts/LayoutWithNavbar';
+import React from 'react';
+
+const Videos = () => {
+	return <div>Videos</div>;
+};
+
+export default Videos;
+
+Videos.getLayout = function getLayout(page: JSX.Element) {
+	return <LayoutWithNavbar>{page}</LayoutWithNavbar>;
+};
