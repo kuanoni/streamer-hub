ceb1d07dd6a79a9a333494e44ec88bbfe3be5a6b
kuanoni <tehgamemaker@gmail.com>
2022-11-22 18:16:23 -0800

    fix merge issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,24 @@
-import { NextComponentType } from 'next';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import AuthorizedPage from '@/components/AuthorizedPage';
 import { NextComponentType, NextPageContext } from 'next';
 import { ReactElement, ReactNode } from 'react';
 
-type LayoutPage = NextComponentType & { getLayout?: (page: ReactElement) => ReactNode, auth: Boolean | undefined };
+type LayoutPage = NextComponentType & { getLayout?: (page: ReactElement) => ReactNode; auth: Boolean | undefined };
 
 interface LayoutAppProps extends AppProps {
 	Component: LayoutPage;
 }
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AuthAppProps) {
-const getLayout = Component.getLayout || ((page) => page);
+export default function App({ Component, pageProps: { session, ...pageProps } }: LayoutAppProps) {
+	const getLayout = Component.getLayout || ((page) => page);
 
 	return (
 		<SessionProvider>
 			{Component.auth ? (
-				<AuthorizedPage>
-					{getLayout(<Component {...pageProps} />)}
-				</AuthorizedPage>
+				<AuthorizedPage>{getLayout(<Component {...pageProps} />)}</AuthorizedPage>
 			) : (
-					{getLayout(<Component {...pageProps} />)}
+				getLayout(<Component {...pageProps} />)
 			)}
 		</SessionProvider>
 	);
