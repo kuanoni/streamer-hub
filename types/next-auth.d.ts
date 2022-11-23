1ecfb9b84f093cf965310be1335dfd452cd4181c
kuanoni <tehgamemaker@gmail.com>
2022-11-23 15:15:48 -0800

    reorganize custom auth types

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -3,17 +3,12 @@ import { DefaultSession } from 'next-auth';
 import { Role } from './custom-auth';
 
 declare module 'next-auth' {
-	/**
-	 * Returned by `useSession`, `getSession` and received as a prop on the
-	 * `SessionProvider` React Context and trpc context
-	 */
-	interface Session {
-		user?: {
-			role?: Role;
-		} & DefaultSession['user'];
+	interface User {
+		role: Role;
+		displayName: string;
 	}
 
-	interface User {
-		role?: Role;
+	interface Session {
+		user: User & DefaultSession['user'];
 	}
 }
