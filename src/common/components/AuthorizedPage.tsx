61f28f97811a5f072ef3d0c261d2f28b757831dc
kuanoni <tehgamemaker@gmail.com>
2022-11-23 08:26:04 -0800

    use comoonent auth to check role

--- src/common/components/AuthorizedPage.tsx
+++ src/common/components/AuthorizedPage.tsx
@@ -1,14 +1,21 @@
 import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { ComponentAuth } from 'types/next-auth';
 
 type Props = {
+	auth: ComponentAuth;
 	children?: React.ReactNode;
 };
 
-const AuthorizedPage = ({ children }: Props) => {
-	const { status } = useSession({ required: true });
+const AuthorizedPage = ({ auth, children }: Props) => {
+	const router = useRouter();
+	const { data, status } = useSession({ required: true });
 
-	if (status === 'loading') {
-		return <div>Loading...</div>;
+	if (status === 'loading' || !data.user) return <>{auth.loading}</>;
+
+	if (data.user.role !== auth.role) {
+		router.push(auth.unauthorized);
+		return <>{auth.loading}</>;
 	}
 
 	return <>{children}</>;
