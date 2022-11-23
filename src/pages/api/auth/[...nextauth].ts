c33009819100ef366800e2b22960e6d80d1e4aed
kuanoni <tehgamemaker@gmail.com>
2022-11-22 18:06:30 -0800

    add mongodb adapter to authOptions

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,12 @@
 import extractStringEnvVar from '@/utils/extractStringEnvVar';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import clientPromise from '@/utils/mongodb';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions = {
+	session: { strategy: 'database' },
+	adapter: MongoDBAdapter(clientPromise),
 	providers: [
 		GoogleProvider({
 			clientId: extractStringEnvVar('GOOGLE_ID'),
