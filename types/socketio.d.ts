1fd624467a7e0324c998b972cab199e21587f71c
kuanoni <tehgamemaker@gmail.com>
2022-11-24 09:02:23 -0800

    Create socketio.d.ts

---
+++ types/socketio.d.ts
@@ -0,0 +1,16 @@
+import { NextApiResponse } from 'next';
+import type { Server as HTTPServer } from 'http';
+import type { Socket as NetSocket } from 'net';
+import { Server as IOServer, Socket } from 'socket.io';
+
+interface SocketServer extends HTTPServer {
+	io?: IOServer | undefined;
+}
+
+interface SocketWithIO extends NetSocket {
+	server: SocketServer;
+}
+
+interface NextApiResponseWithSocket extends NextApiResponse {
+	socket: SocketWithIO;
+}
