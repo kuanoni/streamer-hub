32abbcfe59a8673a40c3bc0b69038c1f3a7f84d7
kuanoni <tehgamemaker@gmail.com>
2022-11-24 15:39:01 -0800

    added simple chat
    - creates socket connection on mount
    - disables message sending when signed out

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,8 +1,63 @@
 import LayoutWithNavbar from '@/layouts/LayoutWithNavbar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Message } from 'types/socketio';
+import SocketIO, { Socket } from 'socket.io-client';
+import { useSession } from 'next-auth/react';
 
 const Chat = () => {
-	return <div>Chat</div>;
+	const { data } = useSession();
+	const [socket, setSocket] = useState<Socket | null>(null);
+	const [messages, setMessages] = useState<Array<Message>>([]);
+	const [outgoingMessage, setOutgoingMessage] = useState<string>('');
+
+	const sendMessage = () => {
+		if (!data?.user) return;
+		const msg: Message = { time: new Date(), displayName: data.user.displayName, message: outgoingMessage };
+		setOutgoingMessage('');
+		socket?.emit('createdMessage', msg);
+	};
+
+	useEffect(() => {
+		let socket: Socket;
+		const socketInitializer = async () => {
+			await fetch('/api/socket');
+		};
+
+		socketInitializer();
+
+		socket = SocketIO();
+
+		socket.on('incomingMessage', (msg: Message) => {
+			setMessages((currentMessages) => [...currentMessages, msg]);
+		});
+
+		setSocket(socket);
+
+		return () => {
+			socket.removeAllListeners();
+			socket.disconnect();
+		};
+	}, []);
+
+	return (
+		<div>
+			<h1>Chat</h1>
+			<input
+				type='text'
+				value={outgoingMessage}
+				onChange={(e) => setOutgoingMessage(e.target.value)}
+				disabled={!data?.user}
+			/>
+			<button onClick={sendMessage} disabled={!data?.user}>
+				Send
+			</button>
+			<div>
+				{messages.map((msg, i) => (
+					<div key={i}>{msg.time + ' | ' + msg.displayName + ' | ' + msg.message}</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Chat;
