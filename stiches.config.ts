53deb6519faa87a0f3406584605f0705e090b9d4
kuanoni <tehgamemaker@gmail.com>
2022-11-25 18:53:25 -0800

    add stiches.config colors

--- stiches.config.ts
+++ stiches.config.ts
@@ -3,6 +3,9 @@ import { createStitches } from '@stitches/react';
 export const { styled, css, globalCss, keyframes, getCssText, theme, createTheme, config } = createStitches({
 	theme: {
 		colors: {
+			bgLightest: '#585858',
+			bgLighter: '#484848',
+			bgLight: '#383838',
 			bg: '#282828',
 			bgDark: '#222',
 			bgDarker: '#111',
