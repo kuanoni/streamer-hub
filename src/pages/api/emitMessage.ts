9fd31ab2057ecc0eb2da5c708a4a0645e912a9cf
kuanoni <tehgamemaker@gmail.com>
2022-11-27 11:01:13 -0800

    hotfix emitMessage

--- src/pages/api/emitMessage.ts
+++ src/pages/api/emitMessage.ts
@@ -2,8 +2,8 @@ import extractStringEnvVar from '@/utils/extractStringEnvVar';
 import { NextApiRequest } from 'next';
 import { NextApiResponseWithSocket } from 'types/socketio';
 
-const emitMessage = () => (req: NextApiRequest, res: NextApiResponseWithSocket) => {
-	if (!res.socket.server.io) return res.end({ status: 500, message: 'Socket server not running' });
+const emitMessage = (req: NextApiRequest, res: NextApiResponseWithSocket) => {
+	if (!res.socket.server.io) return res.send({ status: 500, message: 'Socket server not running' });
 
 	let body;
 	if (typeof req.body === 'object') body = req.body;
