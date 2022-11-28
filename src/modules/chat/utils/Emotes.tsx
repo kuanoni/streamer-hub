f20d73df4431eae53017f8e7c1928f4bef48670c
kuanoni <tehgamemaker@gmail.com>
2022-11-27 22:18:22 -0800

    added emotes

--- src/modules/chat/utils/Emotes.tsx
+++ src/modules/chat/utils/Emotes.tsx
@@ -3,13 +3,13 @@ import { styled } from 'stiches.config';
 import ResidentSleeper from '../../../../public/images/emotes/ResidentSleeper.png';
 import LUL from '../../../../public/images/emotes/LUL.png';
 import BUNGER from '../../../../public/images/emotes/BUNGER.gif';
+import BibleThump from '../../../../public/images/emotes/BibleThump.png';
+import CoolCat from '../../../../public/images/emotes/CoolCat.png';
+import BabyRage from '../../../../public/images/emotes/BabyRage.png';
 
 const StyledEmote = styled(Image, {
 	margin: '-.5rem 0',
 	verticalAlign: 'middle',
-	width: '100%',
-	height: '100%',
-	maxWidth: 32,
 	maxHeight: 32,
 });
 
@@ -21,6 +21,9 @@ export const Emotes: EmotesIface = {
 	ResidentSleeper: (i: string | number) => <StyledEmote key={i} src={ResidentSleeper} alt='ResidentSleeper' />,
 	LUL: (i: string | number) => <StyledEmote key={i} src={LUL} alt='LUL' />,
 	BUNGER: (i: string | number) => <StyledEmote key={i} src={BUNGER} alt='BUNGER' css={{ maxWidth: 48 }} />,
+	BibleThump: (i: string | number) => <StyledEmote key={i} src={BibleThump} alt='BibleThump' />,
+	CoolCat: (i: string | number) => <StyledEmote key={i} src={CoolCat} alt='CoolCat' />,
+	BabyRage: (i: string | number) => <StyledEmote key={i} src={BabyRage} alt='BabyRage' />,
 };
 
 export const EmoteKeys = Object.keys(Emotes);
