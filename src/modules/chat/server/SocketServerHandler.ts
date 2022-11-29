dba44a027209748ba6ef64fb2bdeec3ffc4226e1
kuanoni <tehgamemaker@gmail.com>
2022-11-29 09:38:30 -0800

    add sockets to rooms based on user role

--- src/modules/chat/server/SocketServerHandler.ts
+++ src/modules/chat/server/SocketServerHandler.ts
@@ -1,12 +1,21 @@
 import { Server as IOServer } from 'socket.io';
 import { NextApiResponseWithSocket } from 'types/socketio';
+import { Role } from 'types/custom-auth';
 import { messageHandler } from './ServerEventHandlers';
+import { MessageType } from '../common';
 
 export const SocketServerHandler = (res: NextApiResponseWithSocket) => {
 	if (!res.socket.server.io) {
 		console.log('Starting socket.io server');
 
 		const io = new IOServer(res.socket.server);
+
+		io.use(async (socket, next) => {
+			if (socket.handshake.auth.role === Role.ADMIN) socket.join('admin');
+			if (socket.handshake.auth.role === Role.MOD) socket.join('moderator');
+
+			next();
+		});
 
 		io.on('connection', async (socket) => {
 			const sockets = await io.fetchSockets();
