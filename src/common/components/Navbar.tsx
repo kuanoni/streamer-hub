54eccfab3420c2a4c6d8cc9f82f2d935377cf70b
kuanoni <tehgamemaker@gmail.com>
2022-12-05 11:35:31 -0800

    add UserSignedIn

--- src/common/components/Navbar.tsx
+++ src/common/components/Navbar.tsx
@@ -6,35 +6,15 @@ import SignIn from './SignIn';
 import { AuthPerms } from 'types/custom-auth';
 import { useRouter } from 'next/router';
 import NavButton from './new/NavButton';
+import UserSignedIn from './new/UserSignedIn';
 
 const StyledNav = styled('nav', {
 	display: 'flex',
+	alignItems: 'center',
 	width: '100%',
-	height: 72,
 	color: '$text',
 	borderBottom: '1px solid $textDarker',
 });
-
-const NavButtonLinkStyles = {
-	border: 'none',
-	backgroundColor: 'transparent',
-	textDecoration: 'underline',
-	color: 'inherit',
-	fontSize: 'inherit',
-	cursor: 'pointer',
-	'&:not(:last-child)': {
-		marginRight: '2rem',
-	},
-	'&:hover': {
-		color: '$primary',
-	},
-	'&:active': {
-		color: '$textDark',
-	},
-};
-
-const StyledLink = styled(Link, NavButtonLinkStyles);
-const StyledButton = styled('button', NavButtonLinkStyles);
 
 const Navbar = () => {
 	const { data, status } = useSession();
@@ -61,17 +41,12 @@ const Navbar = () => {
 				<NavButton link='/'>Home</NavButton>
 				<NavButton link='/stream'>Stream</NavButton>
 				<NavButton link='/videos'>Videos</NavButton>
-				{data?.user?.role === AuthPerms.ADMIN && <StyledLink href='/admin'>Admin</StyledLink>}
-				<span className='right'>
-					{status === 'authenticated' ? (
-						<>
-							<StyledButton onClick={() => signOut()}>Sign Out</StyledButton>
-							<StyledLink href='/profile'>Profile</StyledLink>
-						</>
-					) : (
-						<StyledButton onClick={openSignIn}>Sign In</StyledButton>
-					)}
-				</span>
+				{data?.user?.role === AuthPerms.ADMIN && <NavButton link='/admin'>Admin</NavButton>}
+				{status === 'authenticated' ? (
+					<UserSignedIn status={status} />
+				) : (
+					<UserSignedIn status={status} openSignIn={openSignIn} />
+				)}
 			</StyledNav>
 		</>
 	);
