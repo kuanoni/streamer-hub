b49e9265b708e37b2d19d29c2d7dbc29ec1d0361
kuanoni <tehgamemaker@gmail.com>
2022-12-11 19:10:55 -0800

    changed body bg gradient colors

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -30,7 +30,7 @@ const globalStyles = globalCss({
 		padding: 0,
 		color: theme.colors.textLight,
 		background:
-			'radial-gradient(83.01% 75.78% at 80.58% 81.62%, rgba(53, 0, 255, 0.063) 21.39%, rgba(21, 17, 38, 0) 100%), radial-gradient(35.36% 170.61% at 22.03% 20.19%, rgba(126, 148, 255, 0.084) 16.22%, rgba(45, 50, 72, 0) 100%) /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */, #050509',
+			'radial-gradient(83.01% 75.78% at 80.58% 81.62%, rgba(8, 255, 0, 0.04) 21.39%, rgba(33, 17, 38, 0) 100%), radial-gradient(35.36% 170.61% at 22.03% 20.19%, rgba(151, 0, 255, 0.08) 16.22%, rgba(45, 50, 72, 0) 100%) , #090408',
 		fontFamily: `DM Sans, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif`,
 	},
 	a: {
