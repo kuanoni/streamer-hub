3719f0a1121f7be81c7ffa4581af31689bc31b91
kuanoni <tehgamemaker@gmail.com>
2022-12-15 11:38:14 -0800

    Create TextInput.tsx

---
+++ src/common/components/ui/TextInput.tsx
@@ -0,0 +1,48 @@
+import { styled, theme } from 'stiches.config';
+
+const Input = styled('input', {
+	width: '100%',
+	margin: 0,
+	marginLeft: '-.4rem',
+	padding: '.4rem',
+	color: theme.colors.textMedium,
+	backgroundColor: theme.colors.grey900,
+	borderTop: `1px solid ${theme.colors.border}`,
+	borderLeft: `1px solid ${theme.colors.border}`,
+	borderRight: `1px solid ${theme.colors.grey800}`,
+	borderBottom: `1px solid ${theme.colors.grey800}`,
+	borderRadius: theme.space.borderRadHalf,
+	fontSize: '1rem',
+	outlineColor: 'transparent',
+	outlineStyle: 'solid',
+	outlineWidth: 1,
+	outlineOffset: -1,
+	transition: 'outline-color .2s ease',
+
+	'&:focus': {
+		color: theme.colors.textMediumActive,
+		outlineColor: theme.colors.grey400,
+	},
+
+	'&:disabled': {
+		color: theme.colors.textDark,
+		backgroundColor: theme.colors.bg,
+	},
+});
+
+interface Props {
+	value: string;
+	setValue: Function;
+	placeholder: string;
+	disabled?: boolean;
+}
+
+const TextInput = ({ value, setValue, placeholder, disabled = false }: Props) => {
+	const handleChange = (e: React.ChangeEvent) => {
+		setValue(e.target.nodeValue);
+	};
+
+	return <Input type={'text'} value={value} onChange={handleChange} placeholder={placeholder} disabled={disabled} />;
+};
+
+export default TextInput;
