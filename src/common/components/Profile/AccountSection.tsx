cb9e3e17a47f64d9f28b1ffef791cfc2e192d2e7
kuanoni <tehgamemaker@gmail.com>
2022-12-15 11:37:59 -0800

    moved profile section styles to own file

--- src/common/components/Profile/AccountSection.tsx
+++ src/common/components/Profile/AccountSection.tsx
@@ -3,13 +3,7 @@ import { styled, theme } from 'stiches.config';
 
 import ProfileSection from './ProfileSection';
 
-const Label = styled('div', {
-	fontWeight: 700,
-});
 
-const Info = styled('div', {
-	marginBottom: '.5rem',
-});
 
 const AccountSection = () => {
 	return (
---
+++ src/common/components/Profile/styles.ts
@@ -0,0 +1,10 @@
+import { styled } from 'stiches.config';
+
+export const Label = styled('div', {
+	fontWeight: 700,
+	'&:not(:first-child)': {
+		marginTop: '.5rem',
+	},
+});
+
+export const Info = styled('div', {});
