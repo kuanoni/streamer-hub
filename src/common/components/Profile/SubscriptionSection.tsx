b178b9361fcbacbda4955711475360449521126f
kuanoni <tehgamemaker@gmail.com>
2022-12-15 11:38:09 -0800

    Create SubscriptionSection.tsx

---
+++ src/common/components/Profile/SubscriptionSection.tsx
@@ -0,0 +1,12 @@
+import ProfileSection from './ProfileSection';
+import { Info } from './styles';
+
+const SubscriptionSection = () => {
+	return (
+		<ProfileSection title='Subscription'>
+			<Info>You have no active subscriptions.</Info>
+		</ProfileSection>
+	);
+};
+
+export default SubscriptionSection;
