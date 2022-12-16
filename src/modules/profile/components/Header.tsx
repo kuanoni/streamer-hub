a499e513989b8cc0d197a8ccb6db66e4cf5f8dd6
kuanoni <tehgamemaker@gmail.com>
2022-12-16 12:06:14 -0800

    extracted Header into its own component

---
+++ src/modules/profile/components/Header.tsx
@@ -0,0 +1,121 @@
+import { styled, theme } from 'stiches.config';
+
+import DisplayNameInput from '@components/DisplayNameInput';
+import { Rank, User } from '@globalTypes/custom-auth';
+import { RankColors } from '@modules/chat/common';
+
+import { Info, Label, List } from '../styles';
+
+const TopCutout = styled('div', {
+	position: 'absolute',
+	top: 0,
+	left: 0,
+	width: '14rem',
+	height: '6rem',
+	borderTopLeftRadius: theme.space.borderRad,
+	backgroundImage: `radial-gradient(circle at 50% 8.2rem, transparent 6rem, ${theme.colors.primary900} 0)`,
+});
+
+const Header = styled('header', {
+	position: 'absolute',
+	top: 0,
+	left: '14rem',
+	right: 0,
+
+	display: 'flex',
+	justifyContent: 'space-between',
+	alignItems: 'end',
+	height: '6rem',
+	paddingBottom: '.75rem',
+	borderTopRightRadius: theme.space.borderRad,
+	backgroundColor: theme.colors.primary900,
+	fontSize: '2rem',
+});
+
+const Avatar = styled('img', {
+	display: 'block',
+	width: '10rem',
+	height: '10rem',
+	borderRadius: '50%',
+	zIndex: 1,
+});
+
+const DisplayName = styled('h1', {
+	margin: 0,
+	fontSize: '3rem',
+	lineHeight: '1em',
+	variants: { rank: RankColors },
+});
+
+const SubHeader = styled('div', {
+	display: 'flex',
+	alignItems: 'center',
+	gap: '2rem',
+	padding: '2rem',
+	paddingTop: '3rem',
+	paddingBottom: '1rem',
+});
+
+const SubHeaderInfo = styled('div', {
+	paddingTop: '4rem',
+});
+
+interface Props {
+	user: User;
+}
+const ProfileHeader = ({ user }: Props) => {
+	const displayNameMissing = user.displayName === '';
+	const rank =
+		user.rank === Rank.DEFAULT
+			? 'You have no active subscriptions.'
+			: user.rank === Rank.TIER_1
+			? 'Tier 1 Subscriber'
+			: user.rank === Rank.TIER_2
+			? 'Tier 2 Subscriber'
+			: user.rank === Rank.TIER_3
+			? 'Tier 3 Subscriber'
+			: user.rank === Rank.ORBITER
+			? 'Orbiter'
+			: user.rank === Rank.OWNER
+			? 'Owner'
+			: '';
+
+	return (
+		<>
+			<TopCutout />
+			<Header>
+				{displayNameMissing ? (
+					<DisplayNameInput user={user} />
+				) : (
+					<DisplayName rank={user.rank}>{user.displayName}</DisplayName>
+				)}
+			</Header>
+			<SubHeader>
+				<Avatar src={user.avatar} alt='profile picture' referrerPolicy='no-referrer' />
+				<SubHeaderInfo>
+					{displayNameMissing && (
+						<>
+							<Label>Your username must:</Label>
+							<List>
+								<li>Be at least 5 characters long</li>
+								<li>Be at most 15 characters long</li>
+								<li>{'Have no special characters (!?_-.@&$) or spaces'}</li>
+								<li>{'Have no bad words'}</li>
+							</List>
+						</>
+					)}
+					{!displayNameMissing && (
+						<>
+							<Label>Joined</Label>
+							<Info>6th June, 2021 at 21:47 pm</Info>
+							<Label>Subscription</Label>
+							<Info>{rank}</Info>
+						</>
+					)}
+				</SubHeaderInfo>
+			</SubHeader>
+		</>
+	);
+};
+
+export default ProfileHeader;
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -6,8 +6,9 @@ import DisplayNameInput from '@components/DisplayNameInput';
 import { AuthPerms, Rank, User } from '@globalTypes/custom-auth';
 import LayoutWithNavbar from '@layouts/LayoutWithNavbar';
 import { RankColors } from '@modules/chat/common';
-import AccountSection from '@modules/profile/components/AccountSection';
-import SubscriptionSection from '@modules/profile/components/SubscriptionSection';
+import ProfileHeader from '@modules/profile/components/Header';
+import AccountSection from '@modules/profile/components/sections/AccountSection';
+import SubscriptionSection from '@modules/profile/components/sections/SubscriptionSection';
 import { Info, Label, List } from '@modules/profile/styles';
 
 const Container = styled('div', {
@@ -29,60 +30,6 @@ const Container = styled('div', {
         rgba(255, 255, 255, 0.03)`,
 });
 
-const TopCutout = styled('div', {
-	position: 'absolute',
-	top: 0,
-	left: 0,
-	width: '14rem',
-	height: '6rem',
-	borderTopLeftRadius: theme.space.borderRad,
-	backgroundImage: `radial-gradient(circle at 50% 8.2rem, transparent 6rem, ${theme.colors.primary900} 0)`,
-});
-
-const Header = styled('header', {
-	position: 'absolute',
-	top: 0,
-	left: '14rem',
-	right: 0,
-
-	display: 'flex',
-	justifyContent: 'space-between',
-	alignItems: 'end',
-	height: '6rem',
-	paddingBottom: '.75rem',
-	borderTopRightRadius: theme.space.borderRad,
-	backgroundColor: theme.colors.primary900,
-	fontSize: '2rem',
-});
-
-const Avatar = styled('img', {
-	display: 'block',
-	width: '10rem',
-	height: '10rem',
-	borderRadius: '50%',
-	zIndex: 1,
-});
-
-const DisplayName = styled('h1', {
-	margin: 0,
-	fontSize: '3rem',
-	lineHeight: '1em',
-	variants: { rank: RankColors },
-});
-
-const SubHeader = styled('div', {
-	display: 'flex',
-	alignItems: 'center',
-	gap: '2rem',
-	padding: '2rem',
-	paddingTop: '3rem',
-	paddingBottom: '1rem',
-});
-
-const SubHeaderInfo = styled('div', {
-	paddingTop: '4rem',
-});
-
 const ProfileDashboard = () => {
 	const { data } = useSession();
 	const user: User = data?.user;
@@ -91,64 +38,9 @@ const ProfileDashboard = () => {
 
 	const displayNameMissing = user.displayName === '';
 
-	const rank =
-		user.rank === Rank.DEFAULT
-			? 'You have no active subscriptions.'
-			: user.rank === Rank.TIER_1
-			? 'Tier 1 Subscriber'
-			: user.rank === Rank.TIER_2
-			? 'Tier 2 Subscriber'
-			: user.rank === Rank.TIER_3
-			? 'Tier 3 Subscriber'
-			: user.rank === Rank.ORBITER
-			? 'Orbiter'
-			: user.rank === Rank.OWNER
-			? 'Owner'
-			: '';
-
-	const role =
-		user.role === AuthPerms.USER
-			? 'User'
-			: user.role === AuthPerms.MOD
-			? 'Mod'
-			: user.role === AuthPerms.ADMIN
-			? 'Admin'
-			: '';
-
 	return (
 		<Container>
-			<TopCutout />
-			<Header>
-				{displayNameMissing ? (
-					<DisplayNameInput user={user} />
-				) : (
-					<DisplayName rank={user.rank}>{user.displayName}</DisplayName>
-				)}
-			</Header>
-			<SubHeader>
-				<Avatar src={user.avatar} alt='profile picture' referrerPolicy='no-referrer' />
-				<SubHeaderInfo>
-					{displayNameMissing && (
-						<>
-							<Label>Your username must:</Label>
-							<List>
-								<li>Be at least 5 characters long</li>
-								<li>Be at most 15 characters long</li>
-								<li>{'Have no special characters (!?_-.@&$) or spaces'}</li>
-								<li>{'Have no bad words'}</li>
-							</List>
-						</>
-					)}
-					{!displayNameMissing && (
-						<>
-							<Label>Joined</Label>
-							<Info>6th June, 2021 at 21:47 pm</Info>
-							<Label>Subscription</Label>
-							<Info>{rank}</Info>
-						</>
-					)}
-				</SubHeaderInfo>
-			</SubHeader>
+			<ProfileHeader user={user} />
 			<AccountSection user={user} locked={displayNameMissing} />
 			<SubscriptionSection locked={displayNameMissing} />
 		</Container>
