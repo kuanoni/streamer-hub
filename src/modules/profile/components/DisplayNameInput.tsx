798f8bedfdd9a795a55efa9d851e60e58d457313
kuanoni <tehgamemaker@gmail.com>
2022-12-16 23:10:38 -0800

    add db check for name availability + validation

--- src/modules/profile/components/DisplayNameInput.tsx
+++ src/modules/profile/components/DisplayNameInput.tsx
@@ -1,8 +1,7 @@
 import Router from 'next/router';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BsFillHandThumbsUpFill } from 'react-icons/bs';
 import { MoonLoader } from 'react-spinners';
-import PulseLoader from 'react-spinners/PulseLoader';
 import { useDebounce } from 'src/common/hooks/useDebounce';
 import { keyframes, styled, theme } from 'stiches.config';
 
@@ -10,7 +9,7 @@ import { User } from '@globalTypes/custom-auth';
 
 import Button from '../../../common/components/ui/Button';
 import TextInput from '../../../common/components/ui/TextInput';
-import { List } from '../styles';
+import { Label, List } from '../styles';
 
 interface Props {
 	user: User;
@@ -22,10 +21,27 @@ const Container = styled('div', {
 	width: '100%',
 });
 
+const moveIn = keyframes({
+	'0%': {
+		opacity: 0,
+		transform: 'translateY(-2px)',
+	},
+	'100%': {
+		opacity: 1,
+		transform: 'translateY(0px)',
+	},
+});
+
 const ErrorsContainer = styled('div', {
+	position: 'absolute',
+	bottom: 0,
+	paddingTop: '1rem',
 	color: theme.colors.textMedium,
-	fontSize: '.9rem',
-	paddingRight: '2rem',
+	fontSize: '1rem',
+	transform: 'translateY(100%)',
+	'.title, li': {
+		animation: `${moveIn} .2s`,
+	},
 });
 
 const InputContainer = styled('div', {
@@ -54,47 +70,50 @@ const ButtonContainer = styled('div', {
 	animation: `${fadeIn} .2s`,
 });
 
-const displayNameRegex = /^[a-zA-Z0-9 ]+$/;
+type Response200 = { status: 200; available: boolean; validationErrors: string[] };
+type Response500 = { status: 500; message: string };
 
-const validateDisplayName = (name: string) => {
-	const errors = [];
-	if (name.length < 5) errors.push(`At least 5 characters. (${name.length}/5)`);
-	if (name.length > 15) errors.push(`At most 15 characters. (${name.length}/15)`);
-	if (!displayNameRegex.test(name) && name.length) errors.push('Only letters or numbers');
-	if (name.includes(' ')) errors.push('No spaces');
-	return errors;
-};
-
-const checkNameAvailability = async (name: string) => {
-	const res = await fetch(`/api/db/checkDisplayName?name=${name}`, {
+const checkNameAvailability = async (name: string): Promise<Response200 | Response500> => {
+	const res = await fetch(`/api/db/checkDisplayName?displayName=${name}`, {
 		method: 'GET',
 	}).then((res) => res.json());
 
-	console.log(res);
+	return res;
 };
 
 const DisplayNameInput = ({ user }: Props) => {
 	const [displayNameValue, setDisplayNameValue] = useState('');
 	const [errors, setErrors] = useState<string[]>([]);
-	const debouncedDisplayName = useDebounce(displayNameValue, 800);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const debouncedDisplayName: string = useDebounce(displayNameValue, 800);
 	const isDebounced = displayNameValue === debouncedDisplayName;
+	const hasErrors = errors.length !== 0;
+	const showSubmitButton = !isLoading && !hasErrors && displayNameValue.length !== 0 && isDebounced;
 
-	const nameAvailableIndicator = useMemo(async () => {
-		const validationErrors = validateDisplayName(debouncedDisplayName);
-		if (validationErrors.length) return setErrors(validationErrors);
-		const isAvailable = await checkNameAvailability(debouncedDisplayName);
-	}, [debouncedDisplayName]);
+	useEffect(() => {
+		if (debouncedDisplayName === '') return setErrors([]);
 
-	// useEffect(() => {
-	// 	const validationErrors = validateDisplayName(debouncedDisplayName);
-	// 	if (validationErrors.length) return setErrors(validationErrors);
-	// }, [debouncedDisplayName, setErrors]);
+		const newErrors: string[] = [];
+		// spaces cant be sent over an http query, so the validation has to be handled here
+		if (debouncedDisplayName.includes(' ')) newErrors.push('Contains spaces');
+
+		setIsLoading(true);
+		checkNameAvailability(debouncedDisplayName).then((res) => {
+			if (res.status === 200) setErrors([...newErrors, ...res.validationErrors]);
+			else setErrors(newErrors);
+			setIsLoading(false);
+		});
+	}, [debouncedDisplayName, setErrors]);
 
 	const submitDisplayName = async () => {
 		if (displayNameValue !== debouncedDisplayName) return;
 
-		const validationErrors = validateDisplayName(displayNameValue);
-		if (validationErrors.length) return;
+		const availableRes = await checkNameAvailability(displayNameValue);
+
+		if (availableRes.status === 500) return console.log(availableRes.message);
+		if (availableRes.status === 200 && (availableRes.validationErrors.length || !availableRes.available))
+			return console.log(availableRes.validationErrors);
 
 		const res = await fetch('/api/db/userSetDisplayName', {
 			method: 'PATCH',
@@ -117,7 +136,7 @@ const DisplayNameInput = ({ user }: Props) => {
 					color='transparent'
 					size='huge'
 				/>
-				{!errors && isDebounced && (
+				{showSubmitButton && (
 					<ButtonContainer>
 						<Button content='icon' size='fill' onClick={submitDisplayName}>
 							<BsFillHandThumbsUpFill />
@@ -131,6 +150,7 @@ const DisplayNameInput = ({ user }: Props) => {
 				)}
 			</InputContainer>
 			<ErrorsContainer>
+				{hasErrors ? <Label className='title'>Unfortunately, that username...</Label> : null}
 				<List>
 					{errors.map((item) => (
 						<li key={item}>{item}</li>
