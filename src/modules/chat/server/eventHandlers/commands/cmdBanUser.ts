11ac69f1e2914fce9403459725808f77c1a5963a
kuanoni <tehgamemaker@gmail.com>
2022-12-23 06:41:12 -0800

    Create cmdBanUser.ts

---
+++ src/modules/chat/server/eventHandlers/commands/cmdBanUser.ts
@@ -0,0 +1,40 @@
+import add from 'date-fns/add';
+
+import banUser from '@utils/database/banUser';
+
+import ChatCommand from './ChatCommand';
+import { ExecutionCallback } from './types';
+
+const durationRegex = /^(\d\w*)([dhm])$/;
+
+const execCb: ExecutionCallback = async (username, duration?) => {
+	console.log('banning:', username, duration);
+
+	if (duration) {
+		const matches = duration.match(durationRegex);
+		if (!matches) return ['Invalid "duration" format '];
+
+		const [full, amount, timeInterval] = matches;
+
+		switch (timeInterval) {
+			case 'd':
+				return await banUser(username, add(new Date(), { days: parseInt(amount) }));
+			case 'h':
+				return await banUser(username, add(new Date(), { hours: parseInt(amount) }));
+			case 'm':
+				return await banUser(username, add(new Date(), { minutes: parseInt(amount) }));
+		}
+	}
+
+	return await banUser(username);
+};
+
+const cmdBanUser = new ChatCommand('ban')
+	.setDescription('/ban [username] (duration)')
+	.setParams([
+		{ key: 'username', required: true },
+		{ key: 'duration', required: false, regex: durationRegex },
+	])
+	.setExecCb(execCb);
+
+export default cmdBanUser;
