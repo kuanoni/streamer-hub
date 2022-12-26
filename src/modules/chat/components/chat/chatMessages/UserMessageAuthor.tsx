3007e339f27a22f42ec2756df54dcb2987b4b6f1
kuanoni <tehgamemaker@gmail.com>
2022-12-26 12:02:46 -0800

    add all UserFlair variants

--- src/modules/chat/components/chat/chatMessages/UserMessageAuthor.tsx
+++ src/modules/chat/components/chat/chatMessages/UserMessageAuthor.tsx
@@ -4,7 +4,7 @@ import { styled, theme } from 'stiches.config';
 import Tier1 from '@images/flairs/tier_1.png';
 import Tier2 from '@images/flairs/tier_2.png';
 import Tier3 from '@images/flairs/tier_3.png';
-import { UserFlair, UsernameColors } from '@modules/chat/common';
+import { UserFlair } from '@modules/chat/common';
 
 const Container = styled('span', {
 	display: 'inline-flex',
@@ -20,13 +20,22 @@ const Container = styled('span', {
 	},
 	variants: {
 		flair: {
-			t1: {
-				color: theme.colors.grey900,
-				backgroundColor: theme.colors.primary400,
+			[UserFlair.DEFAULT]: {},
+			[UserFlair.TIER_1_SUB]: {
+				color: 'rgb(72, 185, 190)',
 			},
-			t2: {
+			[UserFlair.TIER_2_SUB]: {
+				color: 'rgb(72, 185, 240)',
+			},
+			[UserFlair.TIER_3_SUB]: {
+				color: 'rgb(20, 185, 255)',
+			},
+			[UserFlair.BUDDY]: {
 				color: theme.colors.primary300,
 				backgroundColor: theme.colors.frostedPrimary,
+			},
+			[UserFlair.OWNER]: {
+				color: 'rgb(225, 53, 53)',
 			},
 		},
 	},
@@ -55,7 +64,7 @@ interface Props {
 
 const UserMessageAuthor = ({ flair, onClick, children }: React.PropsWithChildren<Props>) => {
 	return (
-		<Container flair={'t2'} onClick={onClick}>
+		<Container flair={flair} onClick={onClick}>
 			<BadgeContainer>{badges[flair]}</BadgeContainer>
 			{children}
 			{':'}
