78d9184198b1274e683c6709a83c97388da2f5a0
kuanoni <tehgamemaker@gmail.com>
2022-12-26 13:29:41 -0800

    add UserFlair to messages

--- src/modules/chat/server/eventHandlers/sentMessage.ts
+++ src/modules/chat/server/eventHandlers/sentMessage.ts
@@ -1,12 +1,14 @@
 import Joi from 'joi';
+import { User } from 'next-auth';
 import { Socket } from 'socket.io';
 
+import { Role } from '@globalTypes/custom-auth';
 import { UserMessage, UserMessageToServer } from '@globalTypes/socketio';
-import { MessageFlair, SocketEvents, SocketRooms } from '@modules/chat/common';
+import { SocketEvents, SocketRooms, UserFlair } from '@modules/chat/common';
 
 const messageSchema = Joi.object({
 	author: Joi.string().min(5).max(15).required(),
-	flair: Joi.string().valid(...Object.values(MessageFlair)),
+	flair: Joi.string().valid(...Object.values(UserFlair)),
 	time: Joi.date().required(),
 	data: Joi.string().max(500).required(),
 });
@@ -14,9 +16,11 @@ const messageSchema = Joi.object({
 const sentMessage = (socket: Socket) => (msg: UserMessageToServer, callback: Function, room?: SocketRooms) => {
 	if (typeof callback !== 'function') throw new Error("Handler wasn't provided acknowledgement callback");
 
+	const user = socket.user;
+
 	const newMsg: UserMessage = {
-		author: socket.user.username,
-		flair: '',
+		author: user.username,
+		flair: user.selectedFlair,
 		time: new Date().toISOString(),
 		data: msg.data.replace(/\s+/g, ' ').trim(),
 	};
