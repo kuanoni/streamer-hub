e96f5c105c8f88f477968bdf55eb763fd2aa13db
kuanoni <tehgamemaker@gmail.com>
2022-12-29 14:04:52 -0800

    moved messageListReducer to separate file

--- src/modules/chat/components/context/SocketProvider.tsx
+++ src/modules/chat/components/context/SocketProvider.tsx
@@ -7,57 +7,9 @@ import { MessageType } from '@globalTypes/user';
 import createEmbedMessage from '@modules/chat/utils/createEmbedMessage';
 import parseCommandText from '@modules/chat/utils/parseCommandText';
 
-import { DispatchAction, EmbedColors, MessageList, SocketEvents } from '../../common';
+import { EmbedColors, SocketEvents } from '../../common';
+import messageListReducer from './messageListReducer';
 import SocketContext, { SocketProviderIface } from './SocketContext';
-
-const messageListReducer = (state: MessageList, action: DispatchAction): MessageList => {
-	switch (action.type) {
-		case 'push': {
-			return [...state, action.payload];
-		}
-		case 'removeLast': {
-			return [...state.slice(0, state.length - 1)];
-		}
-		case 'updateTextMsg': {
-			const { id, data }: { id: string; data: string } = action.payload;
-
-			const msgIndex = state.findIndex((msg) => msg.id === id);
-			const msg = state[msgIndex];
-
-			if (msg.type !== MessageType.TEXT) {
-				console.log('err');
-
-				return state;
-			}
-
-			const newMsg: UserMessage = { ...msg, data };
-			return [...state.slice(0, msgIndex), newMsg, ...state.slice(msgIndex + 1)];
-		}
-		case 'updateEmbedMsg': {
-			const { id, data }: { id: string; data: EmbedData } = action.payload;
-
-			const msgIndex = state.findIndex((msg) => msg.id === id);
-			const msg = state[msgIndex];
-
-			if (msg.type !== MessageType.EMBED) return state;
-
-			const newData = { ...msg.data, ...data };
-			const newMsg: EmbedMessage = { ...msg, data: newData };
-			return [...state.slice(0, msgIndex), newMsg, ...state.slice(msgIndex + 1)];
-		}
-		case 'replaceMsg': {
-			const { id, newMsg }: { id: string; newMsg: UserMessage | EmbedMessage } = action.payload;
-
-			const msgIndex = state.findIndex((msg) => msg.id === id);
-
-			return [...state.slice(0, msgIndex), newMsg, ...state.slice(msgIndex + 1)];
-		}
-		case 'clear':
-			return [];
-	}
-
-	throw new Error(`Unknown action: ${action}`);
-};
 
 const SocketProvider = ({ children }: { children: React.ReactNode }) => {
 	const { data } = useSession();
