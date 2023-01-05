9fb335d09ec54f2f5a45c4bdffc14ce410f2b869
kuanoni <tehgamemaker@gmail.com>
2023-01-05 07:33:40 -0800

    add user list sections

--- src/modules/chat/components/ChatUsersList.tsx
+++ src/modules/chat/components/ChatUsersList.tsx
@@ -1,13 +1,19 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useMemo, useRef, useState } from 'react';
 import { BsX } from 'react-icons/bs';
 import { styled, theme } from 'stiches.config';
 
+import { Role } from '@globalTypes/user';
 import getUsernameColorsCss from '@utils/getUsernameColorsCss';
 
+import { UsersList, UsersListItem } from '../common';
 import { CloseButton, PopupContainer, PopupContent, PopupHeader, PopupSection } from '../styles';
 import SocketContext from './context/SocketContext';
 
 const Section = styled(PopupSection, {});
+
+const NameContainer = styled('div', {
+	marginBottom: 2,
+});
 
 const Name = styled('span', {
 	padding: '0 .25rem',
@@ -18,13 +24,65 @@ const Name = styled('span', {
 	},
 });
 
+type RoleUsersListItem = UsersListItem & { role: Role };
+
 interface Props {
 	closePopup: () => void;
 }
 
 const ChatUsersList = ({ closePopup }: Props) => {
 	const ctx = useContext(SocketContext);
-	const usersList = ctx?.usersList || [];
+	const [usersListSections, setUsersListSections] = useState<UsersList[]>([]);
+	const mountedRef = useRef<boolean>(false);
+
+	useEffect(() => {
+		if (mountedRef.current || !ctx?.usersList) return;
+		const usersList = ctx.usersList;
+
+		const otherUsers: UsersList = [];
+		const roleUsers: RoleUsersListItem[] = [];
+
+		// split users list
+		for (const user of usersList) {
+			if (user.role) roleUsers.push(user as RoleUsersListItem);
+			else otherUsers.push(user);
+		}
+
+		// sort lists
+		otherUsers.sort((a, b) => a.username.localeCompare(b.username));
+		roleUsers.sort((a, b) => {
+			return a.role.localeCompare(b.role) || a.username.localeCompare(b.username);
+		});
+
+		setUsersListSections([roleUsers, otherUsers]);
+	}, [ctx?.usersList]);
+
+	const sectionComponents = useMemo(
+		() =>
+			usersListSections.map((sectionUsers, i) => {
+				return (
+					<Section key={i}>
+						{sectionUsers.map((user) => {
+							const color = getUsernameColorsCss(user.role, user.subTier);
+							return (
+								<NameContainer key={user.username}>
+									<Name css={color}>{user.username}</Name>
+								</NameContainer>
+							);
+						})}
+					</Section>
+				);
+			}),
+		[usersListSections]
+	);
+
+	// used to prevent
+	useEffect(() => {
+		mountedRef.current = true;
+		return () => {
+			mountedRef.current = false;
+		};
+	}, []);
 
 	return (
 		<PopupContainer>
@@ -34,20 +92,7 @@ const ChatUsersList = ({ closePopup }: Props) => {
 					<BsX />
 				</CloseButton>
 			</PopupHeader>
-			<PopupContent>
-				<Section>
-					{usersList.map((user, i) => {
-						console.log(user);
-
-						const color = getUsernameColorsCss(user.role, user.subTier);
-						return (
-							<div key={user.username}>
-								<Name css={color}>{user.username}</Name>
-							</div>
-						);
-					})}
-				</Section>
-			</PopupContent>
+			<PopupContent>{sectionComponents}</PopupContent>
 		</PopupContainer>
 	);
 };
