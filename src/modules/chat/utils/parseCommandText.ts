07c6f3e39283ca97f5ab134f37f0c5d62796c189
kuanoni <tehgamemaker@gmail.com>
2023-01-05 22:38:47 -0800

    change return type to CommandMessage

--- src/modules/chat/utils/parseCommandText.ts
+++ src/modules/chat/utils/parseCommandText.ts
@@ -1,16 +1,14 @@
-const parseCommandText = (text: string) => {
-	console.log(`cmd text: [${text}]`);
-
+const parseCommandText = (text: string): CommandMessage => {
 	const firstSpaceIdx = text.indexOf(' ');
 
 	if (firstSpaceIdx === -1) {
 		const name = text.slice(1);
 		const params = '';
-		return [name, params];
+		return { name, params };
 	} else {
 		const name = text.slice(1, firstSpaceIdx);
 		const params = text.slice(firstSpaceIdx + 1);
-		return [name, params];
+		return { name, params };
 	}
 };
 
