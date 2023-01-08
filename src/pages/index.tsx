850829bf337a30667bb71c0723e72d3811466a6b
kuanoni <tehgamemaker@gmail.com>
2023-01-08 14:40:42 -0800

    add Sections to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,44 @@
+import { styled } from 'stiches.config';
+
+import Section from '@components/Home/Section';
 import LayoutWithNavbar from '@layouts/LayoutWithNavbar';
 
+const Container = styled('div', {
+	display: 'flex',
+	flexDirection: 'column',
+	gap: '1rem',
+	marginTop: '1rem',
+});
+
+const Row = styled('div', {
+	display: 'flex',
+	gap: '1rem',
+	justifyContent: 'stretch',
+});
+
 export default function Home() {
-	return <div>home</div>;
+	return (
+		<Container>
+			<Section>
+				<Section.Header>Merch</Section.Header>
+				<Section.Content>content</Section.Content>
+			</Section>
+			<Row>
+				<Section>
+					<Section.Header>Twitter</Section.Header>
+					<Section.Content>content</Section.Content>
+				</Section>
+				<Section>
+					<Section.Header>Reddit</Section.Header>
+					<Section.Content>content</Section.Content>
+				</Section>
+			</Row>
+			<Section>
+				<Section.Header>Videos</Section.Header>
+				<Section.Content>content</Section.Content>
+			</Section>
+		</Container>
+	);
 }
 
 Home.getLayout = function getLayout(page: JSX.Element) {
