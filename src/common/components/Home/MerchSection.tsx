f89c7892e820999c3421731688ab75f55a1f58f3
kuanoni <tehgamemaker@gmail.com>
2023-01-09 07:38:16 -0800

    add hover effects to merch links

--- src/common/components/Home/MerchSection.tsx
+++ src/common/components/Home/MerchSection.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { styled } from 'stiches.config';
+import { styled, theme } from 'stiches.config';
 
 import mug from '@images/mug.png';
 import tshirt from '@images/tshirt.png';
@@ -17,9 +17,23 @@ const MerchImage = styled(Image, {
 	width: '100%',
 	height: '100%',
 	objectFit: 'contain',
+	transition: '.1s ease-in',
+	'&:hover': {
+		filter: `drop-shadow(0px 0px 2px ${theme.colors.textLight})`,
+	},
 });
+
 const MerchLink = styled('a', {
 	cursor: 'pointer',
+	transition: '.1s ease-in',
+	'&:hover': {
+		filter: `drop-shadow(0 0 5px ${theme.colors.primary700})`,
+		transform: 'scale(1.05)',
+	},
+	'&:active': {
+		transitionDuration: '0s',
+		transform: 'scale(1) rotateX(15deg)',
+	},
 });
 
 const MerchSection = () => {
