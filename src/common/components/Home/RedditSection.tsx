c63fbbd170b0a918c470a94bd9c3c0ab12f1a79a
kuanoni <tehgamemaker@gmail.com>
2023-01-11 08:11:17 -0800

    add score and comments to reddit posts

--- src/common/components/Home/RedditSection.tsx
+++ src/common/components/Home/RedditSection.tsx
@@ -1,6 +1,8 @@
 import { format, formatDistanceToNowStrict, fromUnixTime } from 'date-fns';
 import Image from 'next/image';
 import { useMemo } from 'react';
+import { BiComment, BiUpvote } from 'react-icons/bi';
+import { BsReddit } from 'react-icons/bs';
 import { styled, theme } from 'stiches.config';
 
 import selfPostThumbnail from '@images/self_post_thumbnail.png';
@@ -14,39 +16,86 @@ const PostThumbnail = styled(Image, {
 	marginTop: '.25em',
 });
 
-const PostTitle = styled('a', {
-	color: theme.colors.textLight,
+const PostTitle = styled('span', {
+	marginBottom: '1.5rem',
+	marginRight: 'auto',
+	paddingRight: '2rem',
+	color: theme.colors.textLink,
 	'&:hover': {
 		textDecoration: 'underline',
 	},
 });
 
 const PostAuthor = styled('div', {
-	marginBottom: '.25em',
 	color: theme.colors.textMedium,
-	fontSize: '0.9em',
+	fontSize: '0.8em',
+});
+
+const PostMetrics = styled('span', {
+	display: 'flex',
+	alignItems: 'center',
+	gap: '1em',
+	svg: {
+		fontSize: '1.25em',
+	},
 });
 
 const PostTime = styled('time', {
-	color: theme.colors.textDark,
-	fontSize: '0.9em',
+	display: 'flex',
+	alignItems: 'center',
+	marginLeft: 'auto',
 });
 
 const PostContent = styled('div', {
 	display: 'flex',
 	flexDirection: 'column',
+	width: '100%',
 });
 
-const Post = styled('article', {
+const PostFooter = styled('footer', {
+	display: 'flex',
+	gap: '1rem',
+	marginTop: 'auto',
+	color: theme.colors.textMedium,
+	fontSize: '0.8em',
+});
+
+const Post = styled('a', {
+	position: 'relative',
 	display: 'flex',
 	columnGap: '1rem',
-	padding: '.5rem',
-	border: `1px solid ${theme.colors.grey900}`,
-	borderBottomLeftRadius: theme.space.borderRad,
-	borderBottomLRightRadius: theme.space.borderRad,
-	'&:not(:last-child)': {
-		borderBottom: 'none',
+	padding: '.75rem 1rem',
+	border: `1px solid ${theme.colors.grey800}`,
+	borderTop: 'none',
+	transition: '.2s ease',
+	'&:hover': {
+		backgroundColor: theme.colors.grey900,
+		textDecoration: 'none',
+		cursor: 'pointer',
 	},
+	'&:first-child': {
+		borderTopLeftRadius: 12,
+		borderTopRightRadius: 12,
+	},
+	'&:last-child': {
+		borderBottom: 'none',
+		borderBottomLeftRadius: 12,
+		borderBottomRightRadius: 12,
+	},
+});
+
+const Container = styled('article', {
+	borderRadius: 12,
+	backgroundColor: 'black',
+	border: `1px solid ${theme.colors.grey800}`,
+});
+
+const RedditIcon = styled('div', {
+	position: 'absolute',
+	top: '.75rem',
+	right: '1rem',
+	color: 'rgb(255, 135, 0)',
+	fontSize: '1.5em',
 });
 
 type Props = {
@@ -57,25 +106,36 @@ type Props = {
 const RedditSection = ({ posts, maxHeight }: Props) => {
 	const postComponents = useMemo(() => {
 		return posts.map((post) => {
-			const { author, title, permalink, thumbnail, created_utc } = post;
+			const { author, title, permalink, thumbnail, created_utc, num_comments, score } = post;
 
 			const timeFromUnix = fromUnixTime(created_utc);
 			const timeAgo = formatDistanceToNowStrict(timeFromUnix);
 			const timeFormatted = format(timeFromUnix, 'yyy-MM-dd, HH:mm:ss');
 
 			return (
-				<Post key={permalink}>
+				<Post key={permalink} href={`https://reddit.com${permalink}`} target='_blank'>
 					{thumbnail === 'self' ? (
 						<PostThumbnail src={selfPostThumbnail} alt='thumbnail' />
 					) : (
 						<PostThumbnail src={thumbnail} alt='thumbnail' width={140} height={140} />
 					)}
 					<PostContent>
-						<PostTitle href={`https://reddit.com${permalink}`} target='_blank'>
-							{title}
-						</PostTitle>
-						<PostAuthor>{`By ${author}`}</PostAuthor>
-						<PostTime title={timeFormatted}>{`${timeAgo} ago`}</PostTime>
+						<RedditIcon>
+							<BsReddit />
+						</RedditIcon>
+						<PostAuthor>/u/{author}</PostAuthor>
+						<PostTitle>{title}</PostTitle>
+						<PostFooter>
+							<PostMetrics>
+								<BiUpvote style={{ marginBottom: '.1em' }} />
+								<span>{score}</span>
+							</PostMetrics>
+							<PostMetrics>
+								<BiComment />
+								<span>{num_comments}</span>
+							</PostMetrics>
+							<PostTime title={timeFormatted}>{`${timeAgo} ago`}</PostTime>
+						</PostFooter>
 					</PostContent>
 				</Post>
 			);
@@ -86,7 +146,7 @@ const RedditSection = ({ posts, maxHeight }: Props) => {
 		<Section>
 			<Section.Header>Reddit</Section.Header>
 			<Section.Content maxHeight={maxHeight} overflowY='auto'>
-				{postComponents}
+				<Container>{postComponents}</Container>
 			</Section.Content>
 		</Section>
 	);
