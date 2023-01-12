62869b1d32abb1395b7838b207a8d3011f043dec
kuanoni <tehgamemaker@gmail.com>
2023-01-12 11:41:06 -0800

    Create VideosSection.tsx

---
+++ src/common/components/Home/VideosSection.tsx
@@ -0,0 +1,103 @@
+import Image from 'next/image';
+import { styled, theme } from 'stiches.config';
+
+import { CSS } from '@stitches/react';
+
+import Section from './Section';
+
+const contentCss: CSS = {
+	padding: '.5rem 1rem',
+};
+
+const RecentVideos = styled('div', {
+	display: 'grid',
+	gridTemplateColumns: 'repeat(3, 1fr)',
+	justifyContent: 'stretch',
+	gap: '1rem',
+});
+
+const RecentVideosTitle = styled('h3', {
+	gridColumn: '1 / -1',
+	margin: '.5rem 0',
+});
+
+const Thumbnail = styled(Image, {
+	display: 'block',
+	width: 'auto',
+	height: 'auto',
+	maxWidth: '100%',
+});
+
+const Feature = styled('article', {
+	width: '100%',
+	marginBottom: '4rem',
+	backgroundColor: theme.colors.grey1000,
+	borderRadius: theme.space.borderRad,
+});
+
+const FeatureThumbnail = styled(Image, {
+	display: 'block',
+	width: 'auto',
+	height: 'auto',
+	maxWidth: '100%',
+});
+
+const FeatureContent = styled('div', {});
+
+const FeatureTitle = styled('h2', {
+	marginTop: '.5rem',
+	marginBottom: 0,
+	whiteSpace: 'nowrap',
+	textOverflow: 'ellipsis',
+	overflow: 'hidden',
+});
+
+const FeatureDescription = styled('p', {
+	margin: 0,
+	color: theme.colors.textMedium,
+});
+
+const featuredVideoId = '-A9Gl9dITk4';
+
+const videoIds = ['adtOYmFdiCc', 'F18gVafNzc0', 'lh9sN4wm2qo', 'Ll9SOTC9o_8', 'HRz_6YUU6Sg', 'bX8IQf2z_Z8'];
+
+interface Props {}
+
+const VideosSection = ({}: Props) => {
+	return (
+		<Section>
+			<Section.Header>Videos</Section.Header>
+			<Section.Content css={contentCss}>
+				<Feature>
+					<FeatureThumbnail
+						src={`https://img.youtube.com/vi/${featuredVideoId}/maxresdefault.jpg`}
+						alt=''
+						width={1280}
+						height={720}
+					/>
+					<FeatureContent>
+						<FeatureTitle>Andrew Tate Self Snitches In Chat Logs, Didn't Expect To Be Caught </FeatureTitle>
+						<FeatureDescription>
+							Destiny reacts to latest Andrew Tate updates from criminal lawyer Bruce Rivers, Romanian
+							TVee and DJ Akademiks...
+						</FeatureDescription>
+					</FeatureContent>
+				</Feature>
+				<RecentVideosTitle>Recent videos</RecentVideosTitle>
+				<RecentVideos>
+					{videoIds.map((id) => (
+						<Thumbnail
+							key={id}
+							src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`}
+							alt=''
+							width={320}
+							height={180}
+						/>
+					))}
+				</RecentVideos>
+			</Section.Content>
+		</Section>
+	);
+};
+
+export default VideosSection;
