f8b6442f7779a0267e2c65c419c727b055d295b1
kuanoni <tehgamemaker@gmail.com>
2023-01-12 07:10:30 -0800

    add merch img hover effect and label

--- src/common/components/Home/MerchSection.tsx
+++ src/common/components/Home/MerchSection.tsx
@@ -6,62 +6,73 @@ import tshirt from '@images/tshirt.png';
 
 import Section from './Section';
 
-const Container = styled('div', {
+const contentCss = {
 	display: 'grid',
 	gridTemplateColumns: 'repeat(6, 1fr)',
 	gridTemplateRows: '1fr',
 	gap: '1rem',
-});
+	padding: '.5rem .25rem',
+};
 
 const MerchImage = styled(Image, {
-	width: '100%',
-	height: '100%',
+	width: '10rem',
+	height: '10rem',
 	objectFit: 'contain',
+	transformOrigin: 'top',
 	transition: '.1s ease-in',
-	'&:hover': {
-		filter: `drop-shadow(0px 0px 2px ${theme.colors.textLight})`,
-	},
+});
+
+const MerchLabel = styled('span', {
+	height: '1.5rem',
+	color: theme.colors.textLight,
+	overflow: 'hidden',
+	transition: 'transform .1s ease',
 });
 
 const MerchLink = styled('a', {
-	maxHeight: '10rem',
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
 	cursor: 'pointer',
-	transition: '.1s ease-in',
 	'&:hover': {
+		textDecoration: 'none',
+	},
+	[`&:hover ${MerchLabel}`]: {
+		transform: 'translateY(-100%)',
+	},
+	[`&:hover ${MerchImage}`]: {
+		transform: 'scale(.87)',
 		filter: `drop-shadow(0 0 5px ${theme.colors.primary700})`,
-		transform: 'scale(1.05)',
 	},
-	'&:active': {
+	[`&:active ${MerchImage}`]: {
 		transitionDuration: '0s',
-		transform: 'scale(1) rotateX(15deg)',
+		transform: 'scale(.87) rotateX(15deg)',
 	},
+});
+
+const MerchItem = styled('div', {
+	height: '10rem',
+	width: '10rem',
+	overflow: 'hidden',
 });
 
 const MerchSection = () => {
 	return (
 		<Section>
 			<Section.Header>Merch</Section.Header>
-			<Section.Content>
-				<Container>
+			<Section.Content css={contentCss}>
+				<MerchItem>
 					<MerchLink href='#'>
 						<MerchImage src={tshirt} alt='tshirt' />
+						<MerchLabel>T-Shirt $35</MerchLabel>
 					</MerchLink>
+				</MerchItem>
+				<MerchItem>
 					<MerchLink href='#'>
 						<MerchImage src={mug} alt='tshirt' />
+						<MerchLabel>Mug $15</MerchLabel>
 					</MerchLink>
-					<MerchLink href='#'>
-						<MerchImage src={tshirt} alt='tshirt' />
-					</MerchLink>
-					<MerchLink href='#'>
-						<MerchImage src={mug} alt='tshirt' />
-					</MerchLink>
-					<MerchLink href='#'>
-						<MerchImage src={tshirt} alt='tshirt' />
-					</MerchLink>
-					<MerchLink href='#'>
-						<MerchImage src={mug} alt='tshirt' />
-					</MerchLink>
-				</Container>
+				</MerchItem>
 			</Section.Content>
 		</Section>
 	);
