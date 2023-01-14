b5f1c92088cc04bc7dcb6610c34328cc4da9e73e
kuanoni <tehgamemaker@gmail.com>
2023-01-14 10:34:27 -0800

    remove min-widths and add xxs bp

--- src/common/hooks/useBreakpoints.ts
+++ src/common/hooks/useBreakpoints.ts
@@ -2,9 +2,10 @@ import useMediaQuery from './useMediaQuery';
 
 const useBreakpoints = () => {
 	const breakpoints = {
+		isXxs: useMediaQuery('(max-width: 375px)'),
 		isXs: useMediaQuery('(max-width: 640px)'),
-		isSm: useMediaQuery('(min-width: 641px) and (max-width: 768px)'),
-		isMd: useMediaQuery('(min-width: 769px) and (max-width: 1024px)'),
+		isSm: useMediaQuery('(max-width: 768px)'),
+		isMd: useMediaQuery('(max-width: 1024px)'),
 		isLg: useMediaQuery('(min-width: 1025px)'),
 		active: 'xs',
 	};
--- stiches.config.ts
+++ stiches.config.ts
@@ -58,9 +58,10 @@ export const { styled, css, globalCss, keyframes, getCssText, theme, createTheme
 		},
 	},
 	media: {
+		xxs: '(max-width: 375px)',
 		xs: '(max-width: 640px)',
-		sm: '(min-width: 641px) and (max-width: 768px)',
-		md: '(min-width: 769px) and (max-width: 1024px)',
+		sm: '(max-width: 768px)',
+		md: '(max-width: 1024px)',
 		lg: '(min-width: 1025px)',
 	},
 	utils: {
