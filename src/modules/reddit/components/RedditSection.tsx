1f07bdb10f31e415feac57d27a3334b58f766dfe
kuanoni <tehgamemaker@gmail.com>
2023-01-14 08:16:41 -0800

    add footer to match Twitter embed

--- src/modules/reddit/components/RedditSection.tsx
+++ src/modules/reddit/components/RedditSection.tsx
@@ -6,43 +6,28 @@ import { CSS } from '@stitches/react';
 
 import RedditPost from './RedditPost';
 
-const Container = styled('div', {
+const Container = styled('article', {
 	border: `1px solid ${theme.colors.grey700}`,
+	fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
 });
 
 const contentCss: CSS = {
 	height: '100%',
 	overflowY: 'auto',
 	borderRight: `1px solid ${theme.colors.grey700}`,
+	cursor: 'pointer',
 };
 
-const Header = styled('div', {
-	position: 'relative',
-	display: 'flex',
-	padding: 12,
-	border: `1px solid ${theme.colors.grey700}`,
-	borderBottom: 'none',
-	borderTopLeftRadius: 12,
-	borderTopRightRadius: 12,
-	fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
-	transition: 'background-color .2s',
-	'&:hover': {
-		backgroundColor: 'rgba(29, 155, 240, 0.1)',
-		cursor: 'pointer',
-	},
-});
-
-const HeaderText = styled('a', {
+const HeaderText = styled('span', {
 	flexShrink: 1,
 	marginTop: 3,
 	color: 'rgb(231, 233, 234)',
 	fontSize: 20,
 	fontWeight: 700,
 	lineHeight: '24px',
-	cursor: 'pointer',
 });
 
-const HeaderLink = styled('a', {
+const HeaderButton = styled('button', {
 	display: 'flex',
 	alignItems: 'center',
 	height: 32,
@@ -50,12 +35,69 @@ const HeaderLink = styled('a', {
 	padding: '0 16px',
 	color: 'rgb(15, 20, 25)',
 	backgroundColor: 'rgb(239, 243, 244)',
+	borderWidth: 0,
 	borderRadius: 9999,
+	fontFamily: 'inherit',
 	fontSize: 14,
 	fontWeight: 700,
-	transition: '.2s',
+	transition: 'background-color .2s',
 	'&:hover': {
 		backgroundColor: 'rgb(215, 219, 220)',
+		textDecoration: 'none',
+		cursor: 'pointer',
+	},
+});
+
+const Header = styled('a', {
+	display: 'flex',
+	padding: 12,
+	border: `1px solid ${theme.colors.grey700}`,
+	borderBottom: 'none',
+	borderTopLeftRadius: 12,
+	borderTopRightRadius: 12,
+	fontFamily: 'inherit',
+	transition: 'background-color .2s',
+	'&:hover': {
+		backgroundColor: 'rgba(29, 155, 240, 0.1)',
+		textDecoration: 'none',
+		cursor: 'pointer',
+	},
+	[`&:hover ${HeaderText}`]: {
+		textDecoration: 'underline',
+	},
+});
+
+const Footer = styled('footer', {
+	padding: '24px 0',
+	border: `1px solid ${theme.colors.grey700}`,
+	borderTop: 'none',
+	borderBottomLeftRadius: 12,
+	borderBottomRightRadius: 12,
+	fontFamily: 'inherit',
+	'&:hover': {
+		textDecoration: 'none',
+		cursor: 'pointer',
+	},
+});
+
+const FooterButton = styled('a', {
+	display: 'flex',
+	alignItems: 'center',
+	width: 'min-content',
+	height: 36,
+	margin: '0 auto',
+	padding: '0 16px',
+	color: '#fff',
+	backgroundColor: 'hsl(32, 100%, 44%)',
+	borderWidth: 0,
+	borderRadius: 9999,
+	fontFamily: 'inherit',
+	fontSize: 15,
+	fontWeight: 700,
+	whiteSpace: 'nowrap',
+	transition: 'background-color .2s',
+	'&:hover': {
+		backgroundColor: 'hsl(32, 100%, 39%)',
 		textDecoration: 'none',
 		cursor: 'pointer',
 	},
@@ -75,15 +117,20 @@ const RedditSection = ({ posts }: Props) => {
 	return (
 		<Section css={{ borderRadius: '11px' }}>
 			<Section.Content css={contentCss}>
-				<Header>
+				<Header href='https://www.reddit.com/r/Destiny/' target='_blank'>
 					<HeaderText>
 						Posts from
 						<br />
 						/r/Destiny
 					</HeaderText>
-					<HeaderLink>Visit</HeaderLink>
+					<HeaderButton>Visit</HeaderButton>
 				</Header>
 				<Container>{postComponents}</Container>
+				<Footer>
+					<FooterButton href='https://www.reddit.com/r/Destiny/' target='_blank'>
+						View more on Reddit
+					</FooterButton>
+				</Footer>
 			</Section.Content>
 		</Section>
 	);
