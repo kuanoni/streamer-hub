17f41443bb482bb3a33012b635c7557a4be84024
kuanoni <tehgamemaker@gmail.com>
2023-01-16 08:06:32 -0800

    add comments, rename vars

--- src/common/utils/database/getUserById.ts
+++ src/common/utils/database/getUserById.ts
@@ -2,14 +2,15 @@ import { User } from 'next-auth';
 
 import clientPromise from '@utils/mongodb';
 
+// find user in db and return data
 const getUserById = async (id: string) => {
 	try {
 		const client = await clientPromise;
 		const db = client.db('auth');
 		const collection = db.collection('users');
 
-		const userDoc = await collection.findOne<User>({ _id: id });
-		return userDoc;
+		const userData = await collection.findOne<User>({ _id: id });
+		return userData;
 	} catch (err) {
 		return null;
 	}
