bf1961796ebdcc7c40c6b8999e83173bc3bb3e52
kuanoni <tehgamemaker@gmail.com>
2023-01-19 12:12:14 -0800

    add social media variables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -51,6 +51,10 @@ const ColumnContainer = styled('div', {
 	'@md': { gap: '1rem' },
 });
 
+const SUBREDDIT_NAME = 'Jerma985';
+const TWITTER_USERNAME = 'Jerma985';
+const YOUTUBE_CHANNEL_ID = 'UCK3kaNXbB57CLcyhtccV_yw';
+
 interface Props {
 	posts: RedditPostData[];
 	videos: YoutubeVideoData[];
