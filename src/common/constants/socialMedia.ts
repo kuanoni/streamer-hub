bef8e5c4d9e760f5f894c1107cc2983a7d46d0c3
kuanoni <tehgamemaker@gmail.com>
2023-01-21 15:16:26 -0800

    Create socialMedia.ts

---
+++ src/common/constants/socialMedia.ts
@@ -0,0 +1,5 @@
+export const SUBREDDIT_NAME = 'WOW';
+export const TWITTER_USERNAME = 'Jerma985';
+// export const YOUTUBE_CHANNEL_ID = 'UCL7DDQWP6x7wy0O6L5ZIgxg';
+// export const YOUTUBE_CHANNEL_ID = 'UCsj_gOOEmCb73tgbjnUdUkg';
+export const YOUTUBE_CHANNEL_ID = 'UC554eY5jNUfDq3yDOJYirOQ';
