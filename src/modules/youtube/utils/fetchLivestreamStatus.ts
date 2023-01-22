c4cc145372daf781437475822d5f4a2b3da73ec5
kuanoni <tehgamemaker@gmail.com>
2023-01-21 16:12:45 -0800

    Delete fetchLivestreamStatus.ts

--- src/modules/youtube/utils/fetchLivestreamStatus.ts
+++
@@ -1,6 +0,0 @@
-const fetchLivestreamStatus = async () => {
-	const data = await fetch('/api/checkLivestreamStatus').then((res) => res.json());
-	return data.live || false;
-};
-
-export default fetchLivestreamStatus;
