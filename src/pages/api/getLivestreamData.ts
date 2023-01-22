15f551669c164d5e8cc8f1a70188887b364cf84c
kuanoni <tehgamemaker@gmail.com>
2023-01-21 16:19:21 -0800

    add proper types

--- src/pages/api/getLivestreamData.ts
+++ src/pages/api/getLivestreamData.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { YOUTUBE_CHANNEL_ID } from 'src/common/constants/socialMedia';
 
 const getVideoId = async () => {
-	return await fetch('https://www.youtube.com/channel/' + YOUTUBE_CHANNEL_ID)
+	const videoId = await fetch('https://www.youtube.com/channel/' + YOUTUBE_CHANNEL_ID)
 		.then((response) => response.text())
 		.then((resp) => {
 			let n = resp.search(/\{"videoId[\sA-Za-z0-9:"\{\}\]\[,\-_]+BADGE_STYLE_TYPE_LIVE_NOW/i);
@@ -25,12 +25,15 @@ const getVideoId = async () => {
 		.catch(function (err) {
 			console.error('Something went wrong', err);
 		});
+
+	return videoId || undefined;
 };
 
 const getLivestreamData = async (req: NextApiRequest, res: NextApiResponse) => {
 	const videoId = await getVideoId();
+	const responseData: LivestreamData = { live: !!videoId, videoId };
 
-	res.send({ status: 200, live: !!videoId, videoId });
+	res.send(responseData);
 	res.end();
 };
 
