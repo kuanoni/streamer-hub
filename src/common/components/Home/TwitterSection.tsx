738a9146f80741906bba1a93f5b38196e1f204f0
kuanoni <tehgamemaker@gmail.com>
2023-01-21 17:37:58 -0800

    add loaders to RedditSection & TwitterSection

--- src/common/components/Home/TwitterSection.tsx
+++ src/common/components/Home/TwitterSection.tsx
@@ -1,22 +1,34 @@
+import { CSSProperties, useState } from 'react';
+import { ClipLoader } from 'react-spinners';
 import { Timeline } from 'react-twitter-widgets';
-import { theme } from 'stiches.config';
+import { styled, theme } from 'stiches.config';
 
 import { CSS } from '@stitches/react';
 
 import Section from '../Section';
 
 const contentCss: CSS = {
+	position: 'relative',
 	height: '100%',
 	overflowY: 'auto',
 	borderRight: `1px solid ${theme.colors.grey700}`,
 	'@sm': { maxWidth: 'calc(100vw - 2rem)' },
 };
 
+const LoaderWrapper = styled('div', {
+	position: 'absolute',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+});
+
 interface Props {
 	username: string;
 }
 
 const TwitterSection = ({ username }: Props) => {
+	const [isLoaded, setItLoaded] = useState(false);
+
 	return (
 		<Section css={{ borderRadius: '11px' }}>
 			<Section.Header></Section.Header>
@@ -32,7 +44,11 @@ const TwitterSection = ({ username }: Props) => {
 						tweetLimit: 10,
 						dnt: true,
 					}}
+					onLoad={() => setItLoaded(true)}
 				/>
+				<LoaderWrapper>
+					<ClipLoader loading={!isLoaded} color={theme.colors.grey300.toString()} size='2rem' />
+				</LoaderWrapper>
 			</Section.Content>
 		</Section>
 	);
--- src/modules/reddit/components/section/RedditSection.tsx
+++ src/modules/reddit/components/section/RedditSection.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from 'react';
-import { styled } from 'stiches.config';
+import ClipLoader from 'react-spinners/ClipLoader';
+import { styled, theme } from 'stiches.config';
 
 import Section from '@components/Section';
 import { CSS } from '@stitches/react';
@@ -15,17 +16,27 @@ const RedditPosts = styled('article', {
 });
 
 const contentCss: CSS = {
+	position: 'relative',
 	height: '100%',
 	overflowY: 'auto',
 	borderRight: `1px solid ${borderColor}`,
 	cursor: 'pointer',
 };
 
+const LoaderWrapper = styled('div', {
+	position: 'absolute',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+});
+
 type Props = {
 	posts: RedditPostData[];
 };
 
 const RedditSection = ({ posts }: Props) => {
+	const hasPosts = posts.length !== 0;
+
 	const postComponents = useMemo(() => {
 		return posts.map((post) => {
 			return <RedditPost key={post.permalink} {...post} />;
@@ -36,9 +47,16 @@ const RedditSection = ({ posts }: Props) => {
 		<Section css={{ borderRadius: '11px' }}>
 			<Section.Header></Section.Header>
 			<Section.Content css={contentCss}>
-				<RedditSectionHeader />
-				<RedditPosts>{postComponents}</RedditPosts>
-				<RedditSectionFooter />
+				{hasPosts ? (
+					<>
+						<RedditSectionHeader />
+						<RedditPosts>{postComponents}</RedditPosts>
+						<RedditSectionFooter />
+					</>
+				) : null}
+				<LoaderWrapper>
+					<ClipLoader loading={!hasPosts} color={theme.colors.grey300.toString()} size='2rem' />
+				</LoaderWrapper>
 			</Section.Content>
 		</Section>
 	);
