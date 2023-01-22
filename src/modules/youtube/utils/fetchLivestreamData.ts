c84e380fb92cad22420cbaa1e076c43f5c5f2b49
kuanoni <tehgamemaker@gmail.com>
2023-01-21 16:19:38 -0800

    fix data returned return

--- src/modules/youtube/utils/fetchLivestreamData.ts
+++ src/modules/youtube/utils/fetchLivestreamData.ts
@@ -1,6 +1,6 @@
 const fetchLivestreamData = async () => {
 	const data = await fetch('/api/getLivestreamData').then((res) => res.json());
-	return data.live || false;
+	return data;
 };
 
 export default fetchLivestreamData;
