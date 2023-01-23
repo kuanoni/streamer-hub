e5c54483be2bf3b8ec15285d2c20f5d50452f257
kuanoni <tehgamemaker@gmail.com>
2023-01-22 20:04:16 -0800

    Update socialMedia.ts

--- src/common/constants/socialMedia.ts
+++ src/common/constants/socialMedia.ts
@@ -1,5 +1,3 @@
-export const SUBREDDIT_NAME = 'WOW';
-export const TWITTER_USERNAME = 'Jerma985';
-// export const YOUTUBE_CHANNEL_ID = 'UCL7DDQWP6x7wy0O6L5ZIgxg';
-// export const YOUTUBE_CHANNEL_ID = 'UCsj_gOOEmCb73tgbjnUdUkg';
-export const YOUTUBE_CHANNEL_ID = 'UC554eY5jNUfDq3yDOJYirOQ';
+export const SUBREDDIT_NAME = 'nextjs';
+export const TWITTER_USERNAME = 'nextjs';
+export const YOUTUBE_CHANNEL_ID = 'UCM2fsEsL6rW99JYMPFmwgtA';
